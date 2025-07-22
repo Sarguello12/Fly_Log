b7faa056887d4eb41c01a8a251546f70caf9ecfb
Samuel Arguello <sam.rene12@gmail.com>
2025-07-21 22:04:15 -0500

    init spring backend

---
+++ backend/src/test/java/com/example/Fly_Log_backend/FlyLogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fly_Log_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyLogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
